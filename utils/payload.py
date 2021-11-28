d8a9204057fa46a73fdc56fbd7d5d7f472a5c860
fahmi-aa <88307266+fahmi-aa@users.noreply.github.com>
2021-11-28 21:48:32 +0700

    feat: payload

---
+++ utils/payload.py
@@ -0,0 +1,13 @@
+from dataclasses import dataclass
+import datetime as dt
+from point import Point
+
+@dataclass
+class Payload:
+    timestamp: dt.datetime
+    id: int
+    type: str
+    location: Point
+
+    def __repr__(self) -> str:
+        return f"Payload(timestamp={self.timestamp}, id={self.id}, type={self.type}, location={self.location})"
