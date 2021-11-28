2d39e4853aacf32af3f20fcc56f5ca7dec29660f
fahmi-aa <88307266+fahmi-aa@users.noreply.github.com>
2021-11-28 21:50:05 +0700

    fix: delete redundant line on test file

--- utils/test_connector.py
+++ utils/test_connector.py
@@ -1,7 +1,6 @@
 from typing import List
 from connector import Connector
 from payload import Payload
-from vehicle import Vehicle
 
 class TestConnector(Connector):
     def __init__(self) -> None:
