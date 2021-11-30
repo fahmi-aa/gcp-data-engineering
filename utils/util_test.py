e24bf3c74d1dcf8c459ecc6164653c16cd663b92
fahmi-aa <88307266+fahmi-aa@users.noreply.github.com>
2021-11-30 21:54:35 +0700

    chore: change test to pubsub

--- utils/util_test.py
+++ utils/util_test.py
@@ -2,14 +2,14 @@ from typing import List
 from point import Point
 from vehicle import Vehicle
 from fleet import Fleet
-from test_connector import TestConnector
+from pubsub_connector import PubSubConnector
 
 vehicles: List[Vehicle] = []
 
-for i in range(20):
+for i in range(10):
     vehicles.append(Vehicle(i, "Dump Truck", Point(0,0), 200))
 
-fleet = Fleet(vehicles, TestConnector())
+fleet = Fleet(vehicles, PubSubConnector("de-porto", "equipment-gps"))
 
 for i in range(20):
     fleet.send_all()
