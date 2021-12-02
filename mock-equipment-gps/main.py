20249f4453fe41654c9cc1cd73f20e1d951c5fb0
fahmi-aa <88307266+fahmi-aa@users.noreply.github.com>
2021-12-02 22:02:07 +0700

    feat: make main to infinite loop

--- mock-equipment-gps/main.py
+++ mock-equipment-gps/main.py
@@ -1,15 +1,51 @@
+import argparse
+from datetime import datetime
+import time
 from typing import List
 from dto import Point
 from vehicle import Vehicle
 from fleet import Fleet
 from connector import PubSubConnector
 
-vehicles: List[Vehicle] = []
+def run(vehicle_num: int, interval: float, project: str, topic_id: str):
+    vehicles: List[Vehicle] = []
+    for i in range(vehicle_num):
+        vehicles.append(Vehicle(i, "Dump Truck", Point(0,0), 200))
 
-for i in range(10):
-    vehicles.append(Vehicle(i, "Dump Truck", Point(0,0), 200))
+    fleet = Fleet(vehicles, PubSubConnector(project, topic_id))
 
-fleet = Fleet(vehicles, PubSubConnector("de-porto", "equipment-gps"))
+    while True:
+        fleet.send_all()
+        print(f"{datetime.now().isoformat()[:-7]}: Data sent")
 
-for i in range(20):
-    fleet.send_all()
+        time.sleep(interval)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--equipment_num",
+        type=int,
+        default=10,
+        help="Number of equipments that has gps"
+    )
+    parser.add_argument(
+        "--interval",
+        type=float,
+        default=15.0,
+        help="Interval of equipment sending data to server"
+    )
+    parser.add_argument(
+        "--project",
+        type=str,
+        default="",
+        help="Project ID"
+    )
+    parser.add_argument(
+        "--topic_id",
+        type=str,
+        default="",
+        help="Pubsub topic ID"
+    )
+    args, _ = parser.parse_known_args()
+
+    run(args.equipment_num, args.interval, args.project, args.topic_id)
---
+++ mock-equipment-gps/start.sh
@@ -0,0 +1,5 @@
+python3 mock-equipment-gps/main.py \
+    --equipment_num 10 \
+    --interval 15 \
+    --project de-porto \
+    --topic_id equipment-gps
