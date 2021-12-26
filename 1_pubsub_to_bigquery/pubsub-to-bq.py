5306c1a68a88bc88a9265f147ab41cb6a752da03
fahmi-aa <88307266+fahmi-aa@users.noreply.github.com>
2021-12-26 21:33:28 +0700

    chore: reformat code

--- 1_pubsub_to_bigquery/pubsub-to-bq.py
+++ 1_pubsub_to_bigquery/pubsub-to-bq.py
@@ -1,8 +1,7 @@
 import json
 import apache_beam as beam
 import datetime as dt
-from google.cloud.bigquery.table import Row
-from apache_beam.options.pipeline_options import PipelineOptions, StandardOptions
+from apache_beam.options.pipeline_options import PipelineOptions
 
 options = PipelineOptions(streaming=True)
 
@@ -11,9 +10,9 @@ pipeline = (
     p
     | "read" >> beam.io.ReadFromPubSub(topic="projects/de-porto/topics/equipment-gps")
     | "bytes to dict" >> beam.Map(lambda x: json.loads(x.decode("utf-8")))
-    # | "windows" >> beam.WindowInto(beam.window.SlidingWindows(30, 5))
-    # | "print" >> beam.Map(lambda x: print(x))
-    | "To BQ Row" >> beam.Map(lambda x: {"timestamp": dt.datetime.utcfromtimestamp(x["timestamp"]).isoformat(), "id": x["id"], "type": x["type"], "x": x["x"], "y": x["y"]})
+    | "To BQ Row" >> beam.Map(
+    lambda x: {"timestamp": dt.datetime.utcfromtimestamp(x["timestamp"]).isoformat(), "id": x["id"],
+               "type": x["type"], "x": x["x"], "y": x["y"]})
     | "store" >> beam.io.WriteToBigQuery("de-porto:de_porto.gps_pubsub")
 )
 
