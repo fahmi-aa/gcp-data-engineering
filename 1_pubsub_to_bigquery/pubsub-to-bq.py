f75d62d4e14539bea6b8c707911b4c6eb996e9a9
fahmi-aa <88307266+fahmi-aa@users.noreply.github.com>
2022-01-15 22:14:05 +0700

    fix: side input

--- 1_pubsub_to_bigquery/pubsub-to-bq.py
+++ 1_pubsub_to_bigquery/pubsub-to-bq.py
@@ -8,13 +8,16 @@ import datetime as dt
 from apache_beam.options.pipeline_options import GoogleCloudOptions
 import logging as log
 
+
 def debug(payload):
     log.info(payload)
     return payload
 
+
 def payload_epoch_to_utc(payload):
     payload["timestamp"] = dt.datetime.utcfromtimestamp(payload["timestamp"]).isoformat()
     return payload
+
 
 def to_wkt_point(payload):
     payload["location"] = f'POINT({payload["y"]} {payload["x"]})'
@@ -23,9 +26,11 @@ def to_wkt_point(payload):
 
     return payload
 
+
 def int_to_date_year(pval):
     pval["year"] = dt.date(pval["year"], 1, 1)
     return pval
+
 
 def enrich_payload(payload, equipments):
     id = payload["id"]
@@ -47,35 +52,35 @@ options = GoogleCloudOptions(streaming=True, save_main_session=True, job_name=f"
 p = beam.Pipeline(options=options)
 
 periodic_side_pipeline = (
-    p
-    | "periodic" >> PeriodicImpulse(fire_interval=3600, apply_windowing=True)
-    | "ApplyGlobalWindow" >> beam.WindowInto(window.GlobalWindows(),
-                                        trigger=trigger.Repeatedly(trigger.AfterProcessingTime(60)),
-                                        accumulation_mode=trigger.AccumulationMode.DISCARDING)
-    | "map to read request" >>
-        beam.Map(lambda x: beam.io.gcp.bigquery.ReadFromBigQueryRequest(table="de-porto:de_porto.equipment"))
-    | beam.io.ReadAllFromBigQuery()
-    | "number to date year1" >> beam.Map(int_to_date_year)
+        p
+        | "periodic" >> PeriodicImpulse(fire_interval=3600, apply_windowing=True)
+        | "ApplyGlobalWindow" >> beam.WindowInto(window.GlobalWindows(),
+                                                 trigger=trigger.Repeatedly(trigger.AfterProcessingTime(60)),
+                                                 accumulation_mode=trigger.AccumulationMode.DISCARDING)
+        | "map to read request" >>
+            beam.Map(lambda x: beam.io.gcp.bigquery.ReadFromBigQueryRequest(table="de-porto:de_porto.equipment"))
+        | beam.io.ReadAllFromBigQuery()
+        | "number to date year1" >> beam.Map(int_to_date_year)
 )
 
 main_pipeline = (
-    p
-    | "read" >> beam.io.ReadFromPubSub(topic="projects/de-porto/topics/equipment-gps")
-    | "bytes to dict" >> beam.Map(lambda x: json.loads(x.decode("utf-8")))
-    | "To wkt point" >> beam.Map(to_wkt_point)
-    | "To BQ Row" >> beam.Map(payload_epoch_to_utc)
-    | "debug1" >> beam.Map(debug)
-    | "timestamp" >> beam.Map(lambda src: window.TimestampedValue(
-        src,
-        dt.datetime.fromisoformat(src["timestamp"]).timestamp()
-    ))
-    | "windowing" >> beam.WindowInto(window.FixedWindows(30))
+        p
+        | "read" >> beam.io.ReadFromPubSub(topic="projects/de-porto/topics/equipment-gps")
+        | "bytes to dict" >> beam.Map(lambda x: json.loads(x.decode("utf-8")))
+        | "To wkt point" >> beam.Map(to_wkt_point)
+        | "To BQ Row" >> beam.Map(payload_epoch_to_utc)
+        | "debug1" >> beam.Map(debug)
+        | "timestamp" >> beam.Map(lambda src: window.TimestampedValue(
+            src,
+            dt.datetime.fromisoformat(src["timestamp"]).timestamp()
+))
+        | "windowing" >> beam.WindowInto(window.FixedWindows(30))
 )
 
 final_pipeline = (
-    main_pipeline
-    | "enrich data" >> beam.FlatMap(enrich_payload, equipments=beam.pvalue.AsIter(periodic_side_pipeline))
-    | "store" >> beam.io.WriteToBigQuery(bq_table)
+        main_pipeline
+        | "enrich data" >> beam.FlatMap(enrich_payload, equipments=beam.pvalue.AsIter(periodic_side_pipeline))
+        | "store" >> beam.io.WriteToBigQuery(bq_table)
 )
 
 result = p.run()
