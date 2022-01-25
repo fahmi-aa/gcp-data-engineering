ee82d2c6cc164f6907906d23d033a4d813fd35c2
fahmi-aa <88307266+fahmi-aa@users.noreply.github.com>
2022-01-25 20:20:02 +0700

    chore: delete script

--- cd-pipeline/dataflow-drain.sh
+++
@@ -1,4 +0,0 @@
-JOB_ID=$(gcloud dataflow jobs list --filter="name~iot-[0-9T:-]{19}" --status=active --limit=1 --format="value(JOB_ID)")
-JOB_ID=${JOB_ID:-"NONE"}
-
-gcloud dataflow jobs drain "${JOB_ID}" --region asia-southeast2
